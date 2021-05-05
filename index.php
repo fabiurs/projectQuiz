f92ae21fbf6e545d71fc5e3480c65696f4121167
Fabian Ursache <69764708+fabiurs@users.noreply.github.com>
2021-05-05 12:10:50 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+// Start the session
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="row">
+            <?php
+            if($_SESSION["loggedin"] == "true")
+                    echo "You are logged in!";
+                else
+                    echo "<a href='login'> Log In</a> <a href='register'>Register</a>";
+             ?>
+        </div>
+    </header>
+
+    <main>
+        <div class="row">
+
+        </div>
+    </main>
+
+    <footer>
+
+    </footer>
+
+</body>
+
+</html>
