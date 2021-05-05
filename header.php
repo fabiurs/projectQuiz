822968717006da0ca73e95438ad3b8d8381a44f2
fabiurs <fabian24ursache@gmail.com>
2021-05-05 12:43:02 +0300

    added header and footer

---
+++ header.php
@@ -0,0 +1,11 @@
+
+<?php
+
+    session_start();
+    function myheader(){
+        if($_SESSION["loggedin"] == "true")
+            echo "a href='account'>My account</a>";
+        else
+            echo "<a href='login'> Log In</a> <a href='register'>Register</a>";
+    }
+?>
