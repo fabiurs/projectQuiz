1fa81814553703352a2cf5feb9b73f615394acea
fabiurs <fabian24ursache@gmail.com>
2021-05-05 12:57:09 +0300

    styled footer

--- index.php
+++ index.php
@@ -3,7 +3,6 @@
 session_start();
 
 include("header.php");
-include("footer.php");
 
 ?>
 
@@ -14,13 +13,14 @@ include("footer.php");
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
+    <title>Online Quiz</title>
+
+    <link rel="stylesheet" href="globalStyle.css">
 
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
-
-    <link rel="stylesheet" href="globalStyle.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous">
 
 </head>
 
@@ -45,7 +45,22 @@ include("footer.php");
     </main>
 
     <footer>
+        <div class="row">
+            <p>© FABIAN URSACHE 2021</p>
+        </div>
 
+        <div class="row">
+            <div class="social-links">
+                <a href="https://www.linkedin.com/in/fabian-ursache/" target="_blank" class="social-image">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+
+                <a href="https://github.com/fabiurs" target="_blank" class="social-image">
+                    <i class="fab fa-github"></i>
+                </a>
+
+            </div>
+        </div>
     </footer>
 
 </body>
